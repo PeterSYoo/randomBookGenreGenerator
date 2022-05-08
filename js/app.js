2137eb8c69af415d68e312dd0e18849fbcfa1343
Peter Yoo <trustintofu@gmail.com>
2022-05-08 14:36:14 -0700

    added randomization of books generated on the page based on subject matter on load and when pressing the generate button

--- js/app.js
+++ js/app.js
@@ -1,29 +1,57 @@
 const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=`
-let genre = `+subject:Science+Fiction&order&orderBy=newest`
-const maxResults = `&maxResults=15`
+const maxResults = `&maxResults=25`
 const key = `&key=AIzaSyA2fp1D9TrSW7x5WJy_gOEkk5ANBF7xS6o`
 const placeHldr = `<img src="https://via.placeholder.com/128">`
 const $outputList = $(`#bookList`);
 
+const genre = [
+  `+subject:CSS&orderBy=newest`,
+  `+subject:HTML&orderBy=newest`,
+  `+subject:Python&orderBy=newest`,
+  `+subject:Javascript&orderBy=newest`,
+]
+
+const shuffle = (array) => {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
+
+const shuffled = () => {
+  const shuf = shuffle(genre);
+  return shuf[0]
+}
+
 $.ajax({
-  url: bookUrl + genre + maxResults + key,
+  url: bookUrl + shuffled(genre) + maxResults + key,
 }).then(
   (data) => {
-    console.log(data.items[1].volumeInfo.imageLinks.thumbnail)
+    $(`#genreH4`).html(`<h4>${data.items[0].volumeInfo.categories}</h4>`)
     console.log(data.items.length)
     const loop = (data) => {for (let i = 0; i < data.items.length; i++) {
-      const bookImg = (data.items[i].volumeInfo.imageLinks) ? data.items[i].volumeInfo.imageLinks.thumbnail : placeHldr;
-      const title = data.items[i].volumeInfo.title
-      const author = data.items[i].volumeInfo.authors
-      const publisher = data.items[i].volumeInfo.publisher
-      const viewUrl = data.items[i].volumeInfo.infoLink
+      const bookImg = () => {
+        if (data.items[i].volumeInfo.imageLinks) {
+          return `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}" class="card-img" alt="...">`
+        } else {
+          return placeHldr;
+        }
+      }
+      const title = data.items[i].volumeInfo.title;
+      const author = data.items[i].volumeInfo.authors;
+      const publisher = data.items[i].volumeInfo.publisher;
+      const viewUrl = data.items[i].volumeInfo.infoLink;
       $outputList.append(`
       <td>
-      <a target="_blank" href="${viewUrl}" class="btn btn-secondary"><img src="${bookImg}" class="card-img" alt="..."></a><br><br>
+      <a target="_blank" href="${viewUrl}">${bookImg()}</a><br><br>
         ${title}<br>
         Author: ${author}<br>
         Publisher: ${publisher}<br>
-        <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
+        <a target="_blank" href="${viewUrl}">Read Book</a>
         <br><br>
       </td>
     `)
@@ -37,25 +65,32 @@ $.ajax({
 );
 
 $(document).on(`click`, `#btnGenerate`, () => {
-  let genre = `+subject:Action`
+  // let genre = `+subject:Action`
   $.ajax({
-    url: bookUrl + genre + maxResults + key,
+    url: bookUrl + shuffled(genre) + maxResults + key,
   }).then(
     (data) => {
-      $outputList.empty()
+      $(`#genreH4`).html(`<h4>${data.items[0].volumeInfo.categories}</h4>`)
+      $outputList.empty();
       const loop = (data) => {for (let i = 0; i < data.items.length; i++) {
-        const bookImg = (data.items[i].volumeInfo.imageLinks) ? data.items[i].volumeInfo.imageLinks.thumbnail : placeHldr;
+        const bookImg = () => {
+          if (data.items[i].volumeInfo.imageLinks) {
+            return `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}" class="card-img" alt="...">`
+          } else {
+            return placeHldr;
+          }
+        }
         const title = data.items[i].volumeInfo.title;
         const author = data.items[i].volumeInfo.authors;
         const publisher = data.items[i].volumeInfo.publisher;
         const viewUrl = data.items[i].volumeInfo.infoLink;
         $outputList.append(`
           <td>
-            <a target="_blank" href="${viewUrl}" class="btn btn-secondary"><img src="${bookImg}" class="card-img" alt="..."></a><br><br>
+            <a target="_blank" href="${viewUrl}">${bookImg()}</a><br><br>
             ${title}<br>
             Author: ${author}<br>
             Publisher: ${publisher}<br>
-            <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
+            <a target="_blank" href="${viewUrl}">Read Book</a>
             <br><br>
           </td>
         `)
