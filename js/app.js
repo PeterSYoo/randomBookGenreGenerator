5f4b9d9f553f97876816abc7dd7050c9858b3e6f
Peter Yoo <trustintofu@gmail.com>
2022-05-08 08:19:46 -0700

    added title, author, publisher, link, book image inside of a <td> and appended it to the page

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
-const $genre1Title = $(`.genre1-content`);
+const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=`
+const genre = `subject:Science+Fiction`
+const key = `&key=AIzaSyA2fp1D9TrSW7x5WJy_gOEkk5ANBF7xS6o`
+const placeHldr = `<img src="https://via.placeholder.com/150">`
+
+const $outputList = $(`#bookList`);
 
 $.ajax({
-  url:'https://www.googleapis.com/books/v1/volumes?q=subject:Science+Fiction&orderBy=newest&key=AIzaSyA2fp1D9TrSW7x5WJy_gOEkk5ANBF7xS6o'
+  url: bookUrl + genre + key,
+  dataType: `json`,
 }).then(
   (data) => {
-   console.log(data.items[0].volumeInfo.title);
-   $genre1Title.text(data.items[0].volumeInfo.title);
+    console.log(data.items[1].volumeInfo.imageLinks.thumbnail)
+    console.log(data.items.length)
+    const loop = (data) => {for (let i = 0; i < data.items.length; i++) {
+      const bookImg = data.items[i].volumeInfo.imageLinks.thumbnail
+      const title = data.items[i].volumeInfo.title
+      const author = data.items[i].volumeInfo.authors
+      const publisher = data.items[i].volumeInfo.publisher
+      const viewUrl = data.items[i].volumeInfo.infoLink
+      $outputList.append(`
+      <td>
+        <img src="${bookImg}" class="card-img" alt="..."><br>
+        ${title}<br>
+        Author: ${author}<br>
+        Publisher: ${publisher}<br>
+        <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
+        <br><br>
+      </td>
+    `)
+    }
+  }
+  loop(data)
   },
   (error) => {
-   console.log('bad request', error);
+    console.log('bad request', error);
   }
 );
-
-// key=AIzaSyA2fp1D9TrSW7x5WJy_gOEkk5ANBF7xS6o
-
