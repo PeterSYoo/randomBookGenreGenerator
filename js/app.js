f0b6c299ef1a0bd1fc7f76e37098f5e92107a51b
Peter Yoo <trustintofu@gmail.com>
2022-05-11 22:52:16 -0700

    shuffle algorithm was always removing 1 element permanently, replaced it with a working algorithm. Added a lot more comments throughout the code.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 const bookUrl = `https://www.googleapis.com/books/v1/volumes?q=`
 const maxResults = `&maxResults=25`
-// const key = `&key=AIzaSyA2fp1D9TrSW7x5WJy_gOEkk5ANBF7xS6o`
 const placeHldr = `<img src="https://via.placeholder.com/128" class="center">`
 const $outputList = $(`#bookList`);
 
-// List of Genre's as elements in an Array
+// List of Genre's as Elements in an Array
 const genre = [
   `+subject:CSS&orderBy=newest`,
   `+subject:HTML&orderBy=newest`,
@@ -33,16 +32,31 @@ const genre = [
   `+subject:Squirrels&orderBy=newest`,
 ]
 
-// Shuffles the genre Array
+// Shuffles the Genre Array
 const shuffle = (array) => {
-  let m = array.length, t, i;
-  while (m) {
-    i = Math.floor(Math.random() * m--);
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
+  let i = array.length;
+  let k; // generate random index
+  let temp; // swap them
+  // i = 8
+  // if 7 is greater than 0 run loop
+  // i decreses by 1 each loop
+  while(--i > 0) {
+                         // lets say 3
+    // Return largest # of (0 through 8)
+    k = Math.floor(Math.random() * (i+1));
+    // k = 3
+    //       [3]
+    temp = array[k];
+    // temp is now [3]
+    array[k] = array[i];
+    // [3] is overwritten by [7]
+    // [`apple`, `orange`, `banana`, `pineapple`, `peach`, `mango`, `grapes`, `pineapple`]
+    array[i] = temp;
+    // [7] is overwritten by [3]
+    // [`apple`, `orange`, `banana`, `pineapple`, `peach`, `mango`, `grapes`, `lemon`]
   }
-  return array;
+  // it does this until --i = 0 then the loop stops
+  return array
 }
 
 // Returns the first index position of the Shuffled Array
@@ -64,19 +78,24 @@ $.ajax({
   url: bookUrl + genreShuffled + maxResults,
 }).then(
   (data) => {
+    // Removes the "_" in the subject string and replaces it with an empty space
     $(`#genreH4`).html(`<h3 id="genreTitle">${sliced.replace(`_`, ` `)}</h4>`)
+    // Loops through the length of the Object from the API
     const loop = (data) => {for (let i = 0; i < data.items.length; i++) {
+      // If Book Cover image is not available, place a Placeholder image instead
       const bookImg = () => {
         if (data.items[i].volumeInfo.imageLinks) {
-          return `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}" class="center" alt="...">`
+          return `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}" class="center">`
         } else {
           return placeHldr;
         }
       }
+      // Binding Variables to Values in the Object from the API that's being iterated
       const title = data.items[i].volumeInfo.title;
       const author = data.items[i].volumeInfo.authors;
       const publisher = data.items[i].volumeInfo.publisher;
       const viewUrl = data.items[i].volumeInfo.infoLink;
+      // Appends onto the DOM
       $outputList.append(`
       <td width="128">
       <a target="_blank" href="${viewUrl}">${bookImg()}</a><br><br>
@@ -92,30 +111,48 @@ $.ajax({
   loop(data)
   },
   (error) => {
-    console.log('bad request', error);
+    console.log(`bad request`, error);
   }
 );
 
 // Clicking Generate button randomly generates Books
 $(document).on(`click`, `#btnGenerate`, () => {
+  // Shuffles the Genre Array
   const shuffle = (array) => {
-    let m = array.length, t, i;
-    while (m) {
-      i = Math.floor(Math.random() * m--);
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
+    let i = array.length;
+    let k; // generate random index
+    let temp; // swap them
+    // i = 8
+    // if 7 is greater than 0 run loop
+    // i decreses by 1 each loop
+    while(--i > 0) {
+                           // lets say 3
+      // Return largest # of (0 through 8)
+      k = Math.floor(Math.random() * (i+1));
+      // k = 3
+      //       [3]
+      temp = array[k];
+      // temp is now [3]
+      array[k] = array[i];
+      // [3] is overwritten by [7]
+      // [`apple`, `orange`, `banana`, `pineapple`, `peach`, `mango`, `grapes`, `pineapple`]
+      array[i] = temp;
+      // [7] is overwritten by [3]
+      // [`apple`, `orange`, `banana`, `pineapple`, `peach`, `mango`, `grapes`, `lemon`]
     }
-    return array;
+    // it does this until --i = 0 then the loop stops
+    return array
   }
-  
+
+  // Returns the first index position of the Shuffled Array
   const shuffled = () => {
     const shuf = shuffle(genre);
     return shuf[0]
   }
-  
+
   const genreShuffled = shuffled(genre);
-  
+
+  // Extracts only the Subject name from the array element
   const sliced = genreShuffled.slice(
     genreShuffled.indexOf(`:`) + 1,
     genreShuffled.lastIndexOf(`&`),
@@ -124,20 +161,24 @@ $(document).on(`click`, `#btnGenerate`, () => {
     url: bookUrl + genreShuffled + maxResults,
   }).then(
     (data) => {
+      // Removes the "_" in the subject string and replaces it with an empty space
       $(`#genreH4`).html(`<h3 id="genreTitle">${sliced.replace(`_`, ` `)}</h4>`)
       $outputList.empty();
+      // If Book Cover image is not available, place a Placeholder image instead
       const loop = (data) => {for (let i = 0; i < data.items.length; i++) {
         const bookImg = () => {
           if (data.items[i].volumeInfo.imageLinks) {
-            return `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}" class="center" alt="...">`
+            return `<img src="${data.items[i].volumeInfo.imageLinks.thumbnail}" class="center">`
           } else {
             return placeHldr;
           }
         }
+        // Binding Variables to Values in the Object from the API that's being iterated
         const title = data.items[i].volumeInfo.title;
         const author = data.items[i].volumeInfo.authors;
         const publisher = data.items[i].volumeInfo.publisher;
         const viewUrl = data.items[i].volumeInfo.infoLink;
+        // Appends onto the DOM
         $outputList.append(`
           <td width="128">
             <a target="_blank" href="${viewUrl}">${bookImg()}</a><br><br>
@@ -153,7 +194,7 @@ $(document).on(`click`, `#btnGenerate`, () => {
     loop(data)
     },
     (error) => {
-      console.log('bad request', error);
+      console.log(`bad request`, error);
     }
   );
 })
